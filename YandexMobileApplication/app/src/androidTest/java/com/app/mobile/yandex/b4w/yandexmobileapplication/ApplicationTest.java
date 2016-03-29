89c549814b8e8721a3146977f19a928c5adf6471
b4w <dzirtdurden@gmail.com>
2016-03-29 22:09:14 +0300

    #1: create android application.

---
+++ YandexMobileApplication/app/src/androidTest/java/com/app/mobile/yandex/b4w/yandexmobileapplication/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.app.mobile.yandex.b4w.yandexmobileapplication;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
